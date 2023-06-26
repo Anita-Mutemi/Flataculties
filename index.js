bf2498eba28b79d3f93cf5a7d2b931ac691d2058
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-06-26 09:04:28 +0300

    editing js html

---
+++ index.js
@@ -0,0 +1,65 @@
+    // Fetch animal data from the server
+    function fetchAnimals() {
+        fetch('http://localhost:3000/characters')
+          .then(response => response.json())
+          .then(data => displayAnimals(data))
+          .catch(error => console.log(error));
+      }
+  
+      // Display the list of animals
+      function displayAnimals(animals) {
+        const animalList = document.getElementById('animal-list');
+        animalList.innerHTML = '';
+  
+        animals.forEach(animal => {
+          const listItem = document.createElement('li');
+          listItem.textContent = animal.name;
+          listItem.addEventListener('click', () => displayAnimalDetails(animal.id));
+          animalList.appendChild(listItem);
+        });
+      }
+  
+      // Display the details of a specific animal
+      function displayAnimalDetails(animalId) {
+        fetch(`http://localhost:3000/characters/${animalId}`)
+          .then(response => response.json())
+          .then(animal => {
+            const animalDetails = document.getElementById('animal-details');
+            animalDetails.innerHTML = `
+              <h3>${animal.name}</h3>
+              <img src="${animal.image}" alt="${animal.name}">
+              <p>Votes: ${animal.votes}</p>
+              <button onclick="addVote(${animal.id})">Add Vote</button>
+            `;
+          })
+          .catch(error => console.log(error));
+      }
+  
+      // Add a vote to an animal
+      function addVote(animalId) {
+        const animalDetails = document.getElementById('animal-details');
+        const votesElement = animalDetails.querySelector('p');
+        const currentVotes = parseInt(votesElement.textContent.split(': ')[1]);
+        const newVotes = currentVotes + 1;
+        votesElement.textContent = `Votes: ${newVotes}`;
+  
+        // Update the votes on the server (not persistent)
+        fetch(`http://localhost:3000/characters/${animalId}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ votes: newVotes })
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+      }
+  
+      // Initialize the app
+      function init() {
+        fetchAnimals();
+      }
+  
+      init();
+    
